bb40fd7df44ed62e3716524e5c6b6d630f8a2ca1
Robin Sliwa <rorrosliwa@gmail.com>
2020-04-09 11:15:31 +0200

    code

---
+++ bot.py
@@ -0,0 +1,30 @@
+import discord
+import json
+
+# Get token
+with open('auth.json') as f:
+    TOKEN = json.load(f)['token']
+
+client = discord.Client()
+
+@client.event
+async def on_message(message):
+    # Prevent from replying to self
+    if message.author == client.user:
+        return
+
+    msg_list = message.content.split(" ")
+
+    if "te" in msg_list or "tea" in msg_list:
+        await client.add_reaction(message, u"\U0001F375")
+    if "kaffe" in msg_list or "coffee" in msg_list:
+        await client.add_reaction(message, u"\u2615")
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+
+client.run(TOKEN)
