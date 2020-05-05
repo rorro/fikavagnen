b22e1bbabb98063a0dde6a36f3ca8170b1ee4331
Robin Sliwa <rorrosliwa@gmail.com>
2020-05-05 23:52:38 +0200

    smile

--- bot.py
+++ bot.py
@@ -7,18 +7,25 @@ with open('auth.json') as f:
 
 client = discord.Client()
 
+thanks_yo = ["tack", "thank", "thx", "tks", "tnx", "10x", "tank", "tx", "thks", "ty"]
+
 @client.event
 async def on_message(message):
     # Prevent from replying to self
     if message.author == client.user:
         return
 
-    msg_list = message.content.split(" ")
+    msg = message.content.lower()
+    msg_list = msg.split(" ")
 
-    if "te" in msg_list or "tea" in msg_list:
+    if "te" in msg or "tea" in msg:
         await message.add_reaction(u"\U0001F375")
-    if "kaffe" in msg_list or "coffee" in msg_list:
+    if "kaffe" in msg or "coffee" in msg:
         await message.add_reaction(u"\u2615")
+    if client.user.mentioned_in(message):
+        for ty in thanks_yo:
+            if ty in msg:
+                await message.add_reaction(u"\U0001F642")
 
 @client.event
 async def on_ready():
