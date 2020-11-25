d5482badf1573f64ba5f595535aea469b39465f3
Robin Sliwa <rorrosliwa@gmail.com>
2020-11-25 16:42:12 +0100

    Add 404 page

--- api.py
+++ api.py
@@ -9,10 +9,6 @@ CRT = '/etc/letsencrypt/live/bestapi.nu/fullchain.pem'
 
 app = Flask(__name__)
 
-@app.route("/")
-def main():
-    return "API not found ;)"
-
 @app.route("/fikavagn", methods=['GET'])
 def serve_fikavagn():
     return render_template("index.html")
@@ -40,6 +36,10 @@ def serve_inner_fika():
         top_tens[emoji_metric] = top_ten_this_metric
 
     return render_template("fika_serving.html", totals=total_data, top=top_tens, fikadags=fikatime)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"), 404
 
 if __name__ == "__main__":
     app.run(debug=False, host="192.168.1.161", port=443, ssl_context=(CRT, KEY))
